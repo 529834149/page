a07ca5246cef6759e3b5bddcb698b449b2da2134
fan-haobai <fanhaobai@gmail.com>
2016-06-01 22:50:39 +0800

    规范代码格式

--- Page.class/Demo.php
+++ Page.class/Demo.php
@@ -12,7 +12,7 @@
  * simple:   1 2 3 4 5 6 7 8 9 10
  * default:  1 2 ... 5 6 7 8 9 ...
  * 
- * 分页样式文件:Page.css,link到需要分页的页面中就OK,修改CSS样式文件可以轻松的得到不同的分页样式
+ * 分页样式文件:Page.css,link到需要分页的页面中就OK，修改CSS样式文件可以轻松的得到不同的分页样式
 */	
 	//设置自动加载
 	spl_autoload_register("autoLoad");
@@ -28,13 +28,13 @@
 	$config = array(
 			 'pagelistsize' => 7   			//翻页列表的大小【不能小于5,默认simple--10 default--7】
 			,'pagename'     => 'page'   //get方式传递和解析翻页参数时的参数名称【默认page】
-			,'theme'		=> 'default'
+			,'theme'				=> 'default'
 			/*主题【默认default】： simple  上一页 1 2 3 4 5 6 7 8 9 10 下一页
 													default 上一页 1 2 ... 5 6 7 8 9 ... 下一页 总页数 跳转到n页 */
-			,'toolbars'     => array('up','down','jump')
+			,'toolbars' 		=> array('up','down','jump')
 			/*工具栏使能【数组形式,需要开启谁就向数组中加入对应值,默认全部使能,simple下使能jump无效】:	
 																						up--上一页 down--下一页 jump--跳转到n页 */
-			,'align'		=> 'center'
+			,'align'				=> 'center'
 		  	/*列表对齐方式【相对于其父元素,默认center】: left--左对齐 center--居中 right--右对齐 */
 		);
 	//获取单例分页对象
